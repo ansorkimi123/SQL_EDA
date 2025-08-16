0fe70b15e5163eec7ceeb3b8d4a857e7d0809bb9
Ansor Hakimi <2022461416@student.uitm.edu.my>
2025-08-16 15:02:39 +0800

    database exploration.sql

--- scripts/database exploration.sql
+++ scripts/database exploration.sql
@@ -4,8 +4,14 @@
 -------------------------------------------------------------
 -------------------------------------------------------------
 --(1) exploration of database
+select *
+from information_schema.tables
 
+--explore all column in the database
+select *
+from information_schema.columns
 
-
-
+select *
+from information_schema.columns
+where TABLE_NAME = 'gold.dim_customers'
 
